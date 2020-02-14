34d8360dba3dc889e534dc2ea13cef86ab09515d
Michael Wells <mwells1286@gmail.com>
2020-02-14 15:41:45 -0800

    update routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import NewBeer from "./components/NewBeer";
 import BeerList from "./components/BeerList";
 import { Switch, Route } from "react-router-dom";
+import Error from "./components/Error";
 import "./fonts.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,16 +13,10 @@ function App() {
   return (
     <div className="App">
       <style jsx>{`
-      .header {
-        box-shadow: 0px 20px 15px;
-      }
-        body {
-    background: #000000;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to bottom, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to bottom, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-
-          );
+  
+      body {
+        background: #000000; 
+        );
         }
       `}</style>
       <div className="header">
@@ -31,9 +25,9 @@ background: linear-gradient(to bottom, #434343, #000000); /* W3C, IE 10+/ Edge, 
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/beerlist" component={BeerList} />
-        <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/newbeer" component={NewBeer} />
+        <Route component={Error} />
       </Switch>
     </div>
   );
