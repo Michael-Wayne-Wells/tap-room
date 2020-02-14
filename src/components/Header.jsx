d1f5e9190d6731d22340257c4c2b13c43bd727da
Michael Wells <mwells1286@gmail.com>
2020-02-14 11:05:27 -0800

    add background image for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import NavigationBar from './NavigationBar'
-import HeaderBody from './HeaderBody'
+import React from "react";
+import NavigationBar from "./NavigationBar";
+import HeaderBody from "./HeaderBody";
+import beer from "../beer.jpeg"
 
 function Header() {
-  return(
-    <div>
-      <NavigationBar/>
-      <HeaderBody/>
+  return (
+    <div className="headerFull">
+      <style jsx>
+        {`
+          .headerFull {
+            background: url(${beer});
+            background-size: 100%;
+            background-repeat: no-repeat;
+            background-position: center;
+          }
+        `}
+      </style>
+      <NavigationBar />
+      <HeaderBody />
     </div>
-  )
+  );
 }
- export default Header
+export default Header;
