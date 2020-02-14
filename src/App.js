891697ffc864897176c06624cdfc725d18d0df33
Michael Wells <mwells1286@gmail.com>
2020-02-14 14:44:17 -0800

    add gradient background

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
-import BeerList from './components/BeerList'
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NewBeer from "./components/NewBeer";
+import BeerList from "./components/BeerList";
 import { Switch, Route } from "react-router-dom";
-import "./fonts.css"
+import "./fonts.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
 function App() {
-
   return (
     <div className="App">
       <style jsx>{`
+      .header {
+        box-shadow: 0px 20px 15px;
+      }
         body {
-          background-color: black;
+    background: #000000;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+          );
         }
       `}</style>
-      <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-      
-          <Route path="/beerlist" component={BeerList} />
-      
-        </Switch>
+      <div className="header">
+        <Header />
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/beerlist" component={BeerList} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/newbeer" component={NewBeer} />
+      </Switch>
     </div>
   );
 }
