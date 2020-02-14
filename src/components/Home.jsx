a14eeb8e91d20df15cfe87d72db0c7292ff480be
Michael Wells <mwells1286@gmail.com>
2020-02-14 15:41:31 -0800

    Add home image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
+import danza from "../danza.gif";
 
 function Home() {
   return (
-    <div>
-      
+    <div className="about">
+      <style jsx>{`
+        .about {
+          height: 30rem;
+        }
+        img {
+          width: 100%;
+        }
+        .danza {
+          margin: 40px auto;
+
+          width: 50%;
+        }
+      `}</style>
+      <div className="danza">
+        <img src={danza} alt="tony danza dancing"></img>
+      </div>
     </div>
   );
 }
