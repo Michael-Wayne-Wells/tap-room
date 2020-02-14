c0b73942de46fd1e5c7cc6c14917d15ea0d52331
Michael Wells <mwells1286@gmail.com>
2020-02-14 10:21:04 -0800

    add table and proptypes for beers.

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -4,19 +4,46 @@ import PropTypes from 'prop-types'
 
 
 function Beer(props) {
-  return(
+  return (
     <div>
-      <style jsx>{`
+      <table>
+        <style jsx>{`
+          table {
+            border: 4px solid black;
+            width: 40%;
+            margin-left: 20px;
+          }
 
-          `}</style>
-
-          BEER!
-      </div>
-    )
+          td {
+            width: 25%;
+          }
+          th {
+            border-bottom: 2px solid black;
+          }
+        `}</style>
+        <tr>
+          <th>Beer</th>
+          <th>Brand</th>
+          <th>flavor</th>
+          <th>IBU</th>
+          <th>ABV</th>
+          <th>Price</th>
+        </tr>
+        <tr>
+          <td> {props.name} </td>
+          <td> {props.brand}</td>
+          <td> {props.flavor}</td>
+          <td> {props.ibu}</td>
+          <td> {props.abv}</td>
+          <td> {props.price} </td>
+        </tr>
+      </table>
+    </div>
+  );
   }
 
   Beer.propTypes = {
-    names: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     brand: PropTypes.string.isRequired,
     price: PropTypes.string,
     flavor: PropTypes.string.isRequired,
--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import Beer from './Beer'
 
+var masterBeerList = [
+  {
+    name: 'Hamms',
+    brand: 'Hamms',
+    price: '$3',
+    flavor: 'Gets the job done.',
+    ibu: "It' a Hamms...",
+    abv: "5%"
+  },
+ 
+];
 
 function BeerList() {
-  return(
+  return (
     <div>
-      <Beer/>
+      {masterBeerList.map((beer, index) =>
+      <Beer name={beer.name}
+      brand={beer.brand}
+      price={beer.price}
+      flavor={beer.flavor}
+      ibu={beer.ibu}
+      abv={beer.ibv}/>
+      )}
     </div>
-  )
+  );
 }
+
  export default BeerList
